d698790ceb6708e1bf03c9f741531e1f5c4b8d3d
xpnew <xpnew@126.com>
2025-06-11 10:16:52 +0800

    提交了网络功能的实现。

---
+++ XP.Util/Net/HostNetInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XP.Util.Net
+{
+
+    [Serializable]
+    public class HostNetInfo
+    {
+
+        public string HostName { get; set; }
+
+        public string HostDomain { get; set; }
+
+        public List<IPInfo> IpList { get; set; } = new List<IPInfo>();
+
+    }
+}
--- XP.Util/Net/IPvsNameTools.cs
+++ XP.Util/Net/IPvsNameTools.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Net;
+using System.Net.NetworkInformation;
+using Microsoft.SqlServer.Server;
 
 
 
@@ -30,6 +32,84 @@ namespace XP.Util.Net
             }
             return AddressIP;
 
+        }
+
+
+        public static HostNetInfo GetHostAndIP()
+        {
+            HostNetInfo Result = new HostNetInfo();
+
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            Result.HostName = properties.HostName;
+            Result.HostDomain = properties.DomainName;
+
+
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (!adapter.Supports(NetworkInterfaceComponent.IPv4) && !adapter.Supports(NetworkInterfaceComponent.IPv6))
+                {
+                    continue;
+                }
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                UnicastIPAddressInformationCollection uniCast = adapterProperties.UnicastAddresses;
+                if (null == uniCast) continue;
+
+                PhysicalAddress physicalAddress = adapter.GetPhysicalAddress();
+                byte[] bytes = physicalAddress.GetAddressBytes();
+                string macAddress = BitConverter.ToString(bytes).Replace("-", ":");
+                Console.WriteLine("MAC ..................:{0}", macAddress);
+                foreach (UnicastIPAddressInformation uni in uniCast)
+                {
+                    if (IPAddress.IsLoopback(uni.Address)) continue;
+                    IPInfo NewItem = new IPInfo()
+                    {
+                        IP = uni.Address.ToString(),
+                        IPv4Mask = uni.IPv4Mask.ToString(),
+                        Type = uni.Address.AddressFamily.ToString(),
+                        Mac = macAddress
+                    };
+                    Result.IpList.Add(NewItem);
+                }
+            }
+            return Result;
+        }
+
+        public static List<IPAddressItem> GetActiveIpAddress()
+        {
+
+            //var lst = NetworkInterface.GetAllNetworkInterfaces().Where(i => i.OperationalStatus == OperationalStatus.Up).Select(i=> new IPAddressItem() { IP =  i.ToString(), 
+            //IsActive= true, Type =  i.Add}).ToList();
+
+
+            var lst = NetworkInterface.GetAllNetworkInterfaces().Where(i => i.OperationalStatus == OperationalStatus.Up).ToList();
+
+            List<IPAddressItem> Result = new List<IPAddressItem>();
+
+            foreach (var ni in lst)
+            {
+
+
+
+            }
+
+
+
+
+
+            return null;
+
+        }
+
+        public static string GetIPType(NetworkInterface ni)
+        {
+            var p = ni.GetIPProperties();
+
+            if (null == p) return "";
+
+
+
+            return "other";
         }
 
         public static string GetHostName()
@@ -58,5 +138,21 @@ namespace XP.Util.Net
             return Result;
         }
 
+
+
+    }
+    public class IPAddressItem
+    {
+        public string HostName { get; set; }
+
+        public string IP { get; set; }
+
+        public string Type { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string IPv4Mask { get; set; }
+
     }
 }
+
