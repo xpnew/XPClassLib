d8675fcdda734c3e24c3f429f1d5bbadebe637fa
xpnew <xpnew@126.com>
2025-06-26 11:31:43 +0800

    补充了一个http请求模式。

--- XP.Util/Http/HttpTool.cs
+++ XP.Util/Http/HttpTool.cs
@@ -342,6 +342,8 @@ namespace XP.Util.Http
                 request.ContentType = "application/x-www-form-urlencoded";
                 request.CookieContainer = cookie;
 
+                //request.Headers.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0");
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0";
 
                 using (Stream postStream = request.GetRequestStream())
                 {
