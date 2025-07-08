b3fb48569284ff16be8bdf4c7f4ba334017386f2
xpnew <xpnew@126.com>
2025-07-08 09:24:23 +0800

    添加了zip使用的dll

---
+++ XP.Util/Net/IPInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XP.Util.Net
+{
+    public class IPInfo
+    {
+
+        public string IP { get; set; }
+
+        public string Type { get; set; }
+
+        public string IPv4Mask { get; set; }
+
+        public string Mac { get; set; }
+
+    }
+}
