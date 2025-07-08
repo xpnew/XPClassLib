38a80ba45b60a784dec4761e5a2545cc91d5f074
xpnew <xpnew@126.com>
2025-07-08 09:04:20 +0800

    修改提交。。。。

--- Win/XP.Win.Components.Monitors/BasicMonitorWin.Designer.cs
+++ Win/XP.Win.Components.Monitors/BasicMonitorWin.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.IsMdiContainer = true;
             this.Name = "BasicMonitorWin";
             this.Text = "基本 监控窗体";
             this.ResumeLayout(false);
