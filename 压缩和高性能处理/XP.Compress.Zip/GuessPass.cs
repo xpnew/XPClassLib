faaaa7f052f62e0783bf8c9ace24d64984668824
xpnew <xpnew@126.com>
2025-07-17 16:22:27 +0800

    zip功能 更新

---
+++ 压缩和高性能处理/XP.Compress.Zip/GuessPass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XP.Compress.Zip
+{
+    /// <summary>
+    ///  猜测密码 GuessPass
+    /// </summary>
+    /// <remarks>
+    /// 创建日期：2025/7/8 16:14:12
+    /// 类名：GuessPass
+    /// 创建人： xpnew@126.com
+    /// </remarks>
+    public class GuessPass :UnZip
+    {
+        #region <属性>
+        #endregion <属性>
+
+        #region <构造方法>
+        #endregion <构造方法>
+
+        #region <内部方法>
+        #endregion <内部方法 end>
+        #region <外部方法>
+
+        public bool TryList(List<string > pwds)
+        {
+
+
+            return false;
+        }
+        #endregion <外部方法 end>
+
+        #region <事件>
+        #endregion <事件>    
+    }
+}
---
+++ 压缩和高性能处理/XP.Compress.Zip/UnZip.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XP.Compress.Zip
+{
+    /// <summary>
+    /// UnZip
+    /// </summary>
+    /// <remarks>
+    /// 创建日期：2025/7/8 11:06:49
+    /// 类名：UnZip
+    /// 创建人： xpnew@126.com
+    /// </remarks>
+    public class UnZip
+    {
+        #region <属性>
+
+        /// <summary>
+        /// 压缩包的完整地址
+        /// </summary>
+        public string FullPath { get; set; }
+
+        /// <summary>
+        /// 解压缩目录 
+        /// </summary>
+        public string ExtFolder { get; set; }
+
+        public UnZip() { }  
+
+
+
+
+        #endregion <属性>
+
+        #region <构造方法>
+        #endregion <构造方法>
+
+        #region <内部方法>
+        #endregion <内部方法 end>
+        #region <外部方法>
+        #endregion <外部方法 end>
+
+        #region <事件>
+        #endregion <事件>    
+    }
+}
